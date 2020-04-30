91071f0990ebad49e6c1766bc377d8c0a6f56c23
evriyanaindrasaputra <eindrasap@gmail.com>
2020-04-30 17:42:31 +0700

    API

--- src/fb.js
+++ src/fb.js
@@ -2,13 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyDweR5X68lIBzshA873oUEoavLteNSozBU",
-  authDomain: "todoindra.firebaseapp.com",
-  databaseURL: "https://todoindra.firebaseio.com",
-  projectId: "todoindra",
-  storageBucket: "todoindra.appspot.com",
-  messagingSenderId: "604366389291",
-  appId: "1:604366389291:web:f567931f08d41ee2232a6e",
+  
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
